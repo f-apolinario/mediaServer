0b2aa4927a16d0065bcf05e34fccf70dd4206897
filipe_apolinario <f.apolinario30@gmail.com>
2015-09-21 08:14:25 +0100

    this is initial commit, in this version of the repository we can send 1 video to multiple clients7

---
+++ src/main.js
@@ -0,0 +1,60 @@
+function intToIP(int) {
+    var part1 = int & 255;
+    var part2 = ((int >> 8) & 255);
+    var part3 = ((int >> 16) & 255);
+    var part4 = ((int >> 24) & 255);
+
+    return part4 + "." + part3 + "." + part2 + "." + part1;
+}
+
+(function main() {
+  var http = require('http');
+  var fs = require('fs');
+  var network = require('network.js');
+  var file = './[720pMkv.Com]_The.Sopranos.S06E21.Members.Only.480p.BluRay.x264-GAnGSteR.webm';
+  //var file = './video.webm'
+  var server = http.createServer(function (req, response) {
+	//This media server has two operations:
+	
+	//list
+    
+	//play
+	
+	var ip = network.discoverLocalIP();
+     
+    if (req.url != '/video.mp4') {
+      console.log(ip);
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.end('<video width="320" height="240" autoplay controls><source src="http://'+ ip +':8030/video.mp4" type="video/mp4"></video>');
+      return;
+    }
+
+    console.log('INFO: ', 'client requested video');
+    fs.stat(file, function (err, stats) {
+      var total = stats.size;
+      var range = req.headers.range;
+      var start = 0, end = total -1;
+      if(range){
+        var positions = range.replace(/bytes=/, "").split("-");
+        end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+        start = parseInt(positions[0], 10);  
+      }
+      var chunksize = (end - start) + 1;
+
+      response.writeHead(206, {
+        "Content-Range": "bytes " + start + "-" + end + "/" + total,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "video/mp4"
+      });
+
+      var stream = fs.createReadStream(file, { start: start, end: end })
+        .on("open", function () {
+          stream.pipe(response);
+        }).on("error", function (err) {
+          response.end(err);
+        });
+    });
+  });
+  server.listen(8030);
+})()  
---
+++ src/network.js
@@ -0,0 +1,17 @@
+var discoverLocalIP = function(){
+  var os = require('os');
+
+  var interfaces = os.networkInterfaces();
+  var addresses = [];
+  for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+      var address = interfaces[k][k2];
+      if (address.family === 'IPv4' && !address.internal) {
+        addresses.push(address.address);
+      }
+    }
+  }
+
+  //console.log('INFO: ', 'this are the local IP addresses: ' + addresses);
+  return addresses[2];
+}
